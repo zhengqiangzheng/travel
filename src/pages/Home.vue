fa7c5eff430cc48d4277de6dacbf501e1e4f339e
zq <2466877980@qq.com>
2019-05-19 18:15:00 +0800

    recommend and weekend compete

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,6 +3,8 @@
   <home-header></home-header>
   <home-swiper></home-swiper>
   <home-icons></home-icons>
+  <home-recommend></home-recommend>
+  <home-weekend></home-weekend>
 </div>
 </template>
 
@@ -10,12 +12,17 @@
 import HomeHeader from './Home/components/Header'
 import HomeSwiper from './Home/components/Swiper'
 import HomeIcons from './Home/components/Icons'
+import HomeRecommend from './Home/components/Recommend'
+import HomeWeekend from './Home/components/Weekend'
 export default {
   name: 'Home',
   components: {
+    HomeWeekend,
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
   }
 }
 </script>
---
+++ src/pages/Home/components/Recommend.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+  <div class="title">
+    热销推荐
+  </div>
+    <ul>
+     <li class="item" v-for="item of recommedList" :key="item.id">
+         <img :src="item.imgUrl" class="item-img">
+       <div class="item-info">
+         <p class="item-title">{{item.title}}</p>
+         <p class="item-desc">{{item.desc}}</p>
+         <button class="item-button">查看详情</button>
+       </div>
+     </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommedList :[{
+        id: '001', imgUrl :'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+        title: '上海迪士尼乐园' ,desc: '全球最大的迪士尼城堡'
+      },{
+        id: '002', imgUrl :'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+        title: '上海迪士尼乐园' ,desc: '全球最大的迪士尼城堡'
+      },{
+        id: '003', imgUrl :'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+        title: '上海迪士尼乐园' ,desc: '全球最大的迪士尼城堡'
+      }]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
+
+</style>
