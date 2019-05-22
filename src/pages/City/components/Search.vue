ef9a44274f1458ddb961ccabb896b6b0ac51f0d0
zq <2466877980@qq.com>
2019-05-22 20:53:10 +0800

    城市搜索框完成

--- src/pages/City/components/Search.vue
+++ src/pages/City/components/Search.vue
@@ -1,12 +1,59 @@
 <template>
+  <div>
   <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+    <input class="search-input" type="text" placeholder="输入城市名或拼音" v-model="keyword"/>
+  </div>
+  <div class="search-content" v-show="keyword">
+    <ul>
+      <li  class="search-item border-bottom" v-for="item of list" :key="item.id">
+          {{item.name}}
+      </li>
+      <li  class="search-item border-bottom" v-show="hasNoData">
+        无匹配数据
+      </li>
+    </ul>
+
+  </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: "CitySearch"
+    name: "CitySearch",
+    props : {
+      cities: Object,
+    },
+    data () {
+      return {
+        keyword: '',
+        timer: null,
+        list : []
+      }
+    },
+    watch : {
+      keyword () {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          const result = []
+          for (let i in this.cities) {
+            this.cities[i].forEach((value) => {
+              if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+                result.push(value)
+              }
+            })
+
+          }
+          this.list = result
+        }, 100)
+      }
+    },
+    computed : {
+      hasNoData () {
+        return !this.list.length
+      }
+    }
   }
 </script>
 <style lang="stylus" scoped>
@@ -23,5 +70,19 @@
       line-height: .62rem
       text-align: center
       border-radius: .06rem
+      color: #666
+  .search-content
+    z-index: 1
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    background: #eee
+    .search-item
+      line-height: .62rem
+      padding-left: .2rem
+      background: #fff
       color: #666
 </style>
