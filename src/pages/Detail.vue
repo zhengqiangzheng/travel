6f86e4401103b4a74077276523100705cf8a1b3a
zq <2466877980@qq.com>
2019-05-28 07:33:53 +0800

    递归组件

--- src/pages/Detail.vue
+++ src/pages/Detail.vue
@@ -2,21 +2,43 @@
   <div>
   <detail-banner></detail-banner>
   <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 <script>
   import DetailBanner from  './Detail/Banner'
   import DetailHeader from  './Detail/Header'
+  import DetailList from  './Detail/List'
 
 
   export default {
     name: 'Detail',
     components: {
       DetailBanner,
-      DetailHeader
+      DetailHeader,
+      DetailList
+    },
+    data (){
+      return{
+        list: [{
+          title: '成人票',
+          children : [
+            {title: '成人3票'},
+            {title: '成人4票'},
+            {title: '成人5票'},
+          ]
+        },
+          {
+            title: '学生票'
+          }, {
+          title: '儿童票'
+        }, {
+          title: '特惠票'
+        },]
+      }
     }
-
   }
 </script>
 
