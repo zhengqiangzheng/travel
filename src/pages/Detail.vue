2b4c5912c569a3dbdf96f14bd1a2b8353dd8e1f7
zq <2466877980@qq.com>
2019-05-27 23:11:07 +0800

    detail-header finish

--- src/pages/Detail.vue
+++ src/pages/Detail.vue
@@ -1,20 +1,29 @@
 <template>
+  <div>
   <detail-banner></detail-banner>
+  <detail-header></detail-header>
+    <div class="content"></div>
+  </div>
 </template>
 <script>
   import DetailBanner from  './Detail/Banner'
+  import DetailHeader from  './Detail/Header'
 
 
   export default {
     name: 'Detail',
     components: {
-      DetailBanner
+      DetailBanner,
+      DetailHeader
     }
 
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="stylus" scoped>
+  .content
+    height 50rem
+
 
 </style>
